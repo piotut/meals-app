8b889e612b3691bf25f5ee310dbdf7d814e06032
Piotr Tutak <piotut@gmail.com>
2019-09-20 08:25:53 +0200

    Screens + navigation

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const CategoryMealsScreen = props => {
+    return (
+        <View style={styles.screen}>
+            <Text>The Category Meals Screen!</Text>
+            <Button
+                title="Go to meal detail"
+                onPress={() => {props.navigation.navigate({routeName: 'MealDetail'});}}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
+
+export default CategoryMealsScreen;
